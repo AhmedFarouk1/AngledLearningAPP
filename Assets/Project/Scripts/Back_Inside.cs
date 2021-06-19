51622538964dfb056e95d2d8520d4818f894d975
Ahmed Farouk <49913499+AhmedFarouk1@users.noreply.github.com>
2021-06-19 18:24:22 +0200

    .

---
+++ Assets/Project/Scripts/Back_Inside.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Back_Inside : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        GameObject panel = GameObject.Find("MathsUnit1_Panel");
+        panel.SetActive(true);
+
+    }
+}
--- Assets/Project/Scripts/ChangeScenes.cs
+++ Assets/Project/Scripts/ChangeScenes.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChangeScenes : MonoBehaviour
-{
+{ 
+   
     public void LoadScene(string sceneName)
     {
+      
        SceneManager.LoadScene(sceneName);
+      
     }
 }
