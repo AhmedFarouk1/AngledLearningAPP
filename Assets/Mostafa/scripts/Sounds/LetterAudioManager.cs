0dc72d726dc05d90e10f065fd1a4c4315a80ec5b
sirius707 <steave_mudblood@yahoo.com>
2021-06-20 22:03:33 +0200

    sound manager

--- Assets/Mostafa/scripts/Sounds/LetterAudioManager.cs
+++ Assets/Mostafa/scripts/Sounds/LetterAudioManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class LetterAudioManager : MonoBehaviour
 {
+    public List<LetterAudio> letteraudio;
+    private AudioSource audiosource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audiosource = GetComponent<AudioSource>();    
     }
 
-    // Update is called once per frame
-    void Update()
+    public void playLetter(string letter)
     {
-        
+        audiosource.clip = letteraudio.Find(audio => audio.Letter == letter).sound;
+        audiosource.Play();
     }
 }
