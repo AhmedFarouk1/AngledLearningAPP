789a8a9772e59ea4c0be981f52a8bbc5edbf4a21
sirius707 <steave_mudblood@yahoo.com>
2021-07-03 15:09:52 +0200

    inside voice in stages 2 and 3

---
+++ Assets/Mostafa/scripts/Sounds/FruitAudio.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class FruitAudio : MonoBehaviour
+{
+    public string fruit_name;
+    public AudioClip sound;
+}
---
+++ Assets/Mostafa/scripts/Sounds/GeneralAudioManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GeneralAudioManager : MonoBehaviour
+{
+    public List<FruitAudio> insidefruitaudios;
+    public List<FruitAudio> outsidefruitaudios;
+
+    public AudioClip insideOrder;
+    public AudioClip outsideOrder;
+
+    public AudioSource audiosource;
+
+    public static GeneralAudioManager _instance;
+    void Awake()
+    {
+
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    public void keepInsideOrder()
+    {
+        audiosource.clip = insideOrder;
+        audiosource.Play();
+    }
+
+    public void keepOutsideOrder()
+    {
+        audiosource.clip = outsideOrder;
+        audiosource.Play();
+    }
+
+    public void playStage2FruitInside(string fruit_name)
+    {
+        fruit_name = fruit_name.ToLower();
+        FruitAudio fruitAudio = insidefruitaudios.Find(audio => audio.fruit_name == fruit_name);
+        if (fruitAudio != null)
+        {
+            audiosource.clip = fruitAudio.sound;
+            audiosource.Play();
+        }
+    }
+
+    public void playStage2FruitOutside(string fruit_name)
+    {
+        fruit_name = fruit_name.ToLower();
+        FruitAudio fruitAudio = outsidefruitaudios.Find(audio => audio.fruit_name == fruit_name);
+        if (fruitAudio != null)
+        {
+            audiosource.clip = fruitAudio.sound;
+            audiosource.Play();
+        }
+    }
+
+}
